0f03243119ec85212f55c760d3c2a355fb22ec95
Marc Abramowitz <marc@marc-abramowitz.com>
2012-05-29 09:13:12 -0700

    Fix a test failure with Jython:
    ```
    Traceback (most recent call last):
      File "/Users/marca/dev/git-repos/stallion/stallion/tests/test_main.py", line 25, in test_get_shared_data_instance_true
        self.assertEqual(data.keys(), ['pypi_update_cache', 'distributions'])
    AssertionError: ['distributions', 'pypi_update_cache'] != ['pypi_update_cache', 'distributions']
    ```

--- stallion/tests/test_main.py
+++ stallion/tests/test_main.py
@@ -22,7 +22,7 @@ class TestMain(unittest.TestCase):
     def test_get_shared_data_instance_true(self):
         data = main.get_shared_data()
         self.assertTrue(isinstance(data, dict))
-        self.assertEqual(data.keys(), ['pypi_update_cache', 'distributions'])
+        self.assertEqual(sorted(data.keys()), ['distributions', 'pypi_update_cache'])
 
     def test_get_pkg_res_instance_true(self):
         pkg = main.get_pkg_res()
