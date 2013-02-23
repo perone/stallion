447d677735aab6b1c1d1101d0c1997ed80a723d0
Christian S. Perone <christian.perone@gmail.com>
2013-02-23 14:06:35 -0300

    Increasing minor version to v0.4

--- stallion/__init__.py
+++ stallion/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['main', 'metadata']
+__all__ = ['main', 'metadata', 'console']
 __author__ = 'Christian S. Perone'
 __author_url__ = 'http://pyevolve.sourceforge.net/wordpress'
-__version__ = '0.3'
+__version__ = '0.4'
