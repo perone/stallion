3522270ace4c09a7546d2d7446fba4ee021ee617
Christian S. Perone <christian.perone@gmail.com>
2013-02-23 17:00:55 -0300

    Implemented the show command.

--- stallion/console.py
+++ stallion/console.py
@@ -17,6 +17,8 @@ from docopt import docopt
 from colorama import init
 from colorama import Fore, Back, Style
 
+from pkg_resources import DistributionNotFound
+
 def ellipsize(msg, max_size=80):
     """This function will ellipsize the string.
 
@@ -49,6 +51,10 @@ def parse_dict(mdata, key, ellip=False):
     except KeyError:
         return 'Not Specified'
 
+def get_kv_colored(key, value):
+    text = Fore.WHITE + Style.BRIGHT + '  %s: ' % key.capitalize()
+    text += Fore.WHITE + Style.NORMAL + value
+    return text
 
 def get_field_formatted(mdata, key):
     """ This function will get the formatted and colored 
@@ -58,13 +64,95 @@ def get_field_formatted(mdata, key):
     :param key: the key of the dict
     :return: the formatted and colored string
     """
+    
+    def recursive_dict(d, depth=2, final=''):
+        final_str = final
+        for k,v in sorted(d.items(), key=lambda x: x[0]):
+            if isinstance(v, dict):
+                if depth==2:
+                    final_str += Fore.BLUE + Style.BRIGHT
+                else:
+                    final_str += Fore.WHITE + Style.NORMAL
+
+                final_str += '  ' * depth + str(k) + '\n'
+                final_str += recursive_dict(v, depth+1, final)
+            else:
+                final_str =+ ("  ")*depth + str(k) + ' ' + str(v) + '\n'
+        return final_str
+
     field = parse_dict(mdata, key.lower())
+    
     if isinstance(field, list):
         field = ', '.join(field)
 
-    text = Fore.WHITE + Style.BRIGHT + '  %s: ' % key
-    text += Fore.WHITE + Style.NORMAL + field
+    if isinstance(field, dict):
+        field = '\n\n' + recursive_dict(field)
+
+    text = get_kv_colored(key, field)
     return text
+
+def cmd_show(args):
+    """This function implements the package show command.
+
+    :param args: the docopt parsed arguments
+    """
+    proj_name = args['<project_name>']
+
+    try:
+        pkg_dist = get_pkg_res().get_distribution(proj_name)
+    except:
+        print Fore.RED + Style.BRIGHT + 'Error: unable to locate the project \'%s\' !' % proj_name
+        print Fore.RESET + Back.RESET + Style.RESET_ALL
+        return
+
+    pkg_metadata = pkg_dist.get_metadata(metadata.METADATA_NAME)
+    parsed, key_known = metadata.parse_metadata(pkg_metadata)
+    distinfo = metadata.metadata_to_dict(parsed, key_known)
+
+    proj_head = Fore.GREEN + Style.BRIGHT + pkg_dist.project_name
+    proj_head += Fore.YELLOW + Style.BRIGHT + ' ' + pkg_dist.version
+    print proj_head,
+
+    proj_sum = Fore.WHITE + Style.DIM
+    proj_sum += '- ' + parse_dict(distinfo, 'summary', True)
+    print proj_sum
+
+    # Remove long fields and used fields
+    if 'description' in distinfo:
+        del distinfo['description']
+
+    if 'summary' in distinfo:
+        del distinfo['summary']
+
+    if 'name' in distinfo:
+        del distinfo['name']
+
+    if 'version' in distinfo:
+        del distinfo['version']
+
+    classifier = None
+    if 'classifier' in distinfo:
+        classifier = distinfo['classifier']
+        del distinfo['classifier']
+
+    for key in distinfo:
+        print get_field_formatted(distinfo, key)
+
+    print
+    print get_kv_colored('location', pkg_dist.location)
+    requires = pkg_dist.requires()
+
+    if len(requires) == 0:
+        print get_kv_colored('requires', 'none')
+    else:
+        req_text = '\n'
+        for req in requires:
+            req_text += ' '*4 + str(req) + '\n'
+        print get_kv_colored('requires', req_text)
+
+    distinfo['classifier'] = classifier
+    print get_field_formatted(distinfo, 'classifier')
+
 
 def cmd_list(args):
     """This function implements the package list command.
@@ -108,6 +196,7 @@ def run_main():
 
     Usage:
       plp list [<filter>]
+      plp show <project_name>
 
       plp (-h | --help)
       plp --version
@@ -125,6 +214,9 @@ def run_main():
 
     if arguments['list']:
         cmd_list(arguments)
+
+    if arguments['show']:
+        cmd_show(arguments)
         
 if __name__ == '__main__':
     init()
